6de6af63952b12452f779c99038eb06349f4ccca
Hai Van Vo <haivanvowork@gmail.com>
2026-04-15 12:11:37 +0700

    Update alignment.sh

--- scripts/alignment.sh
+++ scripts/alignment.sh
@@ -2,6 +2,5 @@ minimap2 -ax map-ont \
   -t 4 \
   /path/to/ref.fasta \
   output/sample_filtered.fastq.gz \
-
-samtools sort -o output/sample.bam
-samtools index output/sample.bam
+  | samtools sort -o output/sample.bam
+  samtools index output/sample.bam
